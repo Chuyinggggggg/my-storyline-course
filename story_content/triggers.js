5c1b866c19a113339410148b7a2c45bc8a6bb08d
Chuyinggggggg <cocoyuanluck@gmail.com>
2025-03-26 14:47:02 -0400

    Add files via upload

---
+++ story_content/triggers.js
@@ -0,0 +1,41 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6WOJpYneSdv":
+        Script1();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+window.Script1 = function()
+{
+  const target = object('5scedQ5ExrQ');
+const duration = 3000;
+const easing = 'ease-out';
+const id = '6HZfVEIfMBj';
+const shakeAmount = 5;
+player.addForTriggers(
+id,
+target.animate([
+{ translate: '0 0' },
+{ translate: `-${shakeAmount}px 0` },
+{ translate: '0 0' },
+{ translate: `${shakeAmount}px 0` },
+{ translate: '0 0' },
+{ translate: `-${shakeAmount}px 0` },
+{ translate: '0 0' }
+],
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+};
